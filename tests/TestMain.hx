7eeadfec09317a610a6c861c2963bc36ebcac689
AlexHaxe <Alexander.Blum@gmail.com>
2024-05-30 23:24:32 +0200

    added LCOV data for unittest runs

--- tests/TestMain.hx
+++ tests/TestMain.hx
@@ -1,4 +1,5 @@
 import sys.FileSystem;
+import sys.io.File;
 import coverage.TestCoverage;
 import utest.ITest;
 import utest.Runner;
@@ -10,11 +11,24 @@ class TestMain {
 		final runner:Runner = new Runner();
 		runner.onComplete.add(_ -> {
 			FileSystem.deleteFile("tests/.missing-data.json");
+			collectLcovData();
 		});
 		new DiagnosticsReport(runner);
 		for (test in tests) {
 			runner.addCase(test());
 		}
 		runner.run();
+	}
+
+	static function collectLcovData() {
+		var lcovData:Array<String> = [];
+		for (fileName in FileSystem.readDirectory(Sys.getCwd())) {
+			if (!~/lcov.*\.test\.info/.match(fileName)) {
+				continue;
+			}
+			lcovData.push(File.getContent(fileName));
+			FileSystem.deleteFile(fileName);
+		}
+		File.saveContent("lcovTest.info", lcovData.join("\n"));
 	}
 }
